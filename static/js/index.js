32f9e0c8f7f50f948b2a12e73ecc13762a5dc8d6
Sam Cohen <cohens@rpi.edu>
2018-09-15 23:30:56 -0400

    5:00 AM FALLBACK POINT
    Revert here if not working at 5:00 AM

--- static/js/index.js
+++ static/js/index.js
@@ -13,68 +13,68 @@ map.on('load', function() {
         "data": datasrc
     });
 
-    map.addLayer({
-        "id": "earthquakes-heat",
-        "type": "heatmap",
-        "source": "fuel-type-colors",
-        "maxzoom": 16.38,
-        "paint": {
-            // Increase the heatmap weight based on frequency and property magnitude
-            "heatmap-weight": [
-                "interpolate",
-                ["linear"],
-                ["get", "rate-of-spread"],
-                0, 0.25,
-                100, 1
-            ],
-            // Increase the heatmap color weight weight by zoom level
-            // heatmap-intensity is a multiplier on top of heatmap-weight
-            "heatmap-intensity": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                0, 1,
-                9, 3
-            ],
-            // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-            // Begin color ramp at 0-stop with a 0-transparancy color
-            // to create a blur-like effect.
-            "heatmap-color": [
-                "interpolate",
-                ["linear"],
-                ["heatmap-density"],
-                0, "rgba(33,102,172,0)",
-                0.2, "rgb(103,169,207)",
-                0.4, "rgb(209,229,240)",
-                0.6, "rgb(253,219,199)",
-                0.8, "rgb(239,138,98)",
-                1, "rgb(178,24,43)"
-            ],
-            // Adjust the heatmap radius by zoom level
-            "heatmap-radius": [
-                "interpolate",
-                ["linear"],
-                ["get","rate-of-spread"],
-                0, 10,
-                100, 20
-            ],
-            // Transition from heatmap to circle layer by zoom level
-            "heatmap-opacity": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                7, 1,
-                20, 0.25
-            ],
-        }
-    });
+    // map.addLayer({
+    //     "id": "earthquakes-heat",
+    //     "type": "heatmap",
+    //     "source": "fuel-type-colors",
+    //     "maxzoom": 16.38,
+    //     "paint": {
+    //         // Increase the heatmap weight based on frequency and property magnitude
+    //         "heatmap-weight": [
+    //             "interpolate",
+    //             ["linear"],
+    //             ["get", "rate-of-spread"],
+    //             0, 0.25,
+    //             100, 1
+    //         ],
+    //         // Increase the heatmap color weight weight by zoom level
+    //         // heatmap-intensity is a multiplier on top of heatmap-weight
+    //         "heatmap-intensity": [
+    //             "interpolate",
+    //             ["linear"],
+    //             ["zoom"],
+    //             0, 1,
+    //             9, 3
+    //         ],
+    //         // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+    //         // Begin color ramp at 0-stop with a 0-transparancy color
+    //         // to create a blur-like effect.
+    //         "heatmap-color": [
+    //             "interpolate",
+    //             ["linear"],
+    //             ["heatmap-density"],
+    //             0, "rgba(33,102,172,0)",
+    //             0.2, "rgb(103,169,207)",
+    //             0.4, "rgb(209,229,240)",
+    //             0.6, "rgb(253,219,199)",
+    //             0.8, "rgb(239,138,98)",
+    //             1, "rgb(178,24,43)"
+    //         ],
+    //         // Adjust the heatmap radius by zoom level
+    //         "heatmap-radius": [
+    //             "interpolate",
+    //             ["linear"],
+    //             ["get","rate-of-spread"],
+    //             0, 10,
+    //             100, 20
+    //         ],
+    //         // Transition from heatmap to circle layer by zoom level
+    //         "heatmap-opacity": [
+    //             "interpolate",
+    //             ["linear"],
+    //             ["zoom"],
+    //             7, 1,
+    //             20, 0.25
+    //         ],
+    //     }
+    // });
 
 
     map.addLayer({
         "id": "fuel-points",
         "type": "circle",
         "source": "fuel-type-colors",
-        "minzoom": 16.38,
+        //"minzoom": 16.38,
         "paint": {
             // Color circle by earthquake magnitude
             "circle-color": [
@@ -126,5 +126,5 @@ map.on('load', function() {
 })
 
 setInterval(function() {
-    console.log(map.getZoom())
+    //console.log(map.getZoom())
 }, 100);
